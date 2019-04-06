7fccf2052b80973305b8aac9c8a7c90341694551
Johannes Hoppe <info@johanneshoppe.com>
2019-04-06 14:45:23 +0200

    Remove pdr

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python
+import subprocess
+
 from setuptools import setup
 
-setup(
-    setup_requires=['pbr'],
-    pbr=True,
-)
+
+def get_version():
+    try:
+        output = subprocess.check_output(  # nosec
+            ['git', 'describe', '--tags']
+        ).decode().strip()
+    except subprocess.CalledProcessError:
+        pass
+    else:
+        try:
+            tag, commit_num, sha = output.split('-', 3)
+        except ValueError:
+            pep440_version = output
+        else:
+            pep440_version = '%s.dev%s' % (tag, commit_num)
+
+        return pep440_version
+
+
+setup(version=get_version())
