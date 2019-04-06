e5c673bb7a4c3d7b6cad14a3b584c28ce1a0efe7
Johannes Hoppe <info@johanneshoppe.com>
2019-04-06 22:53:20 +0200

    Add pbr for release veriosn

--- setup.py
+++ setup.py
@@ -1,25 +1,7 @@
 #!/usr/bin/env python
-import subprocess
-
 from setuptools import setup
 
-
-def get_version():
-    try:
-        output = subprocess.check_output(  # nosec
-            ['git', 'describe', '--tags']
-        ).decode().strip()
-    except subprocess.CalledProcessError:
-        pass
-    else:
-        try:
-            tag, commit_num, sha = output.split('-', 3)
-        except ValueError:
-            pep440_version = output
-        else:
-            pep440_version = '%s.dev%s' % (tag, commit_num)
-
-        return pep440_version
-
-
-setup(version=get_version())
+setup(
+    setup_requires=['pbr'],
+    pbr=True,
+)
